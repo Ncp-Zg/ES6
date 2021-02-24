1ff0f5e3618e96f84b2495e0c5d3ee103449412a
Necip <ncptzgn@gmail.com>
2021-02-24 10:48:39 +0300

    ES6 Özellikleri

---
+++ arrow.js
@@ -0,0 +1,27 @@
+// const merhaba = function (){
+//     console.log("merhaba")
+// }
+
+// merhaba();
+
+// Arrow function
+
+// const merhaba = () => {
+//     console.log ("Merhaba");
+// }
+// merhaba();
+
+
+// const merhaba = (firstName,secondName) => console.log("Merhaba",firstName,secondName);
+
+// merhaba("Murat","Mehmet");
+
+// const cube = function(x){
+//     return x * x *x;
+
+// }
+// console.log(cube(4));
+
+
+const cube = x => x*x*x;
+console.log(cube(4));
