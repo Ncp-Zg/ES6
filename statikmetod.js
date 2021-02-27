46de089441f617d5204711ee87b73765f2003f3b
Necip <ncptzgn@gmail.com>
2021-02-27 09:36:33 +0300

    ES6 Statik Metod

---
+++ statikmetod.js
@@ -0,0 +1,25 @@
+// Statik Metodlar
+
+// class Matematik {
+//     sqrt(x){
+//         console.log(x*x);
+//     }
+//     static cube(x) {
+//         console.log(x*x*x);
+//     }
+// }
+// // const math = new Matematik();
+// // math.cube(3);
+// // console.log(math);
+
+// const math = new Matematik();
+// Matematik.cube(3);
+// console.log(math);
+// // math.cube(4);
+
+// math.sqrt(4);
+
+// Matematik.sqrt(4);
+
+// Object.create();
+console.log(Math.sqrt(4));
